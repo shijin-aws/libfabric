194d2840f07ddc15e295ccd3cd87ea1921277542
Shi Jin <sjina@amazon.com>
2023-08-09 23:23:28 +0000

    fabtests/pytest/efa: extend the test_unexpected_msg test
    Cover different msg size for different protocols
    and different in flight message numbers
    
    Signed-off-by: Shi Jin <sjina@amazon.com>

--- fabtests/pytest/efa/test_unexpected_msg.py
+++ fabtests/pytest/efa/test_unexpected_msg.py
@@ -1,7 +1,18 @@
 import pytest
+from efa.efa_common import efa_run_client_server_test
+
+
+SHM_DEFAULT_MAX_INJECT_SIZE = 4096
+SHM_DEFAULT_RX_SIZE = 1024
+
 
 @pytest.mark.functional
-def test_unexpected_msg(cmdline_args):
+@pytest.mark.parametrize("msg_size", [1, 512, 9000, 1048576]) # cover various switch points of shm/efa protocols
+@pytest.mark.parametrize("msg_count", [1, 1024, 2048]) # below and above efa/shm's default rx size
+def test_unexpected_msg(cmdline_args, msg_size, msg_count):
     from common import ClientServerTest
-    test = ClientServerTest(cmdline_args, "fi_unexpected_msg -e rdm -I 10")
-    test.run()
+    if msg_size > SHM_DEFAULT_MAX_INJECT_SIZE and msg_count > SHM_DEFAULT_RX_SIZE and cmdline_args.server_id == cmdline_args.client_id:
+        pytest.skip("SHM's CMA/SAR protocol currently cannot handle > rx size number of unexpected messages")
+    efa_run_client_server_test(cmdline_args, f"fi_unexpected_msg -e rdm -M {msg_count}", iteration_type="short",
+                               completion_semantic="transmit_complete", memory_type="host_to_host",
+                               message_size=msg_size, completion_type="queue")
