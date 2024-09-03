3b98fb5156a9b31a41ccf83415de09ad9ceb8993
sjina <sjina@ip-10-0-4-36.corp.pcluster.com>
2024-09-03 20:28:26 +0000

    prov/efa: Logging qp lifecycles in libfabric.

--- prov/efa/src/rdm/efa_rdm_cq.c
+++ prov/efa/src/rdm/efa_rdm_cq.c
@@ -470,12 +470,14 @@ void efa_rdm_cq_poll_ibv_cq(ssize_t cqe_to_process, struct efa_ibv_cq *ibv_cq)
 
 	while (!err) {
 		pkt_entry = (void *)(uintptr_t)ibv_cq->ibv_cq_ex->wr_id;
+		EFA_WARN(FI_LOG_CQ, "polling cq for qpn: %d, efa_rdm_cq: %p\n", ibv_wc_read_qp_num(ibv_cq->ibv_cq_ex), efa_rdm_cq);
 		qp = efa_domain->qp_table[ibv_wc_read_qp_num(ibv_cq->ibv_cq_ex) & efa_domain->qp_table_sz_m1];
 		ep = container_of(qp->base_ep, struct efa_rdm_ep, base_ep);
 		efa_rdm_tracepoint(poll_cq, (size_t) ibv_cq->ibv_cq_ex->wr_id);
 		opcode = ibv_wc_read_opcode(ibv_cq->ibv_cq_ex);
 		if (ibv_cq->ibv_cq_ex->status) {
 			prov_errno = efa_rdm_cq_get_prov_errno(ibv_cq->ibv_cq_ex);
+			EFA_WARN(FI_LOG_CQ, "hit cq error, qpn: %d, prov_errno: %d, efa_rdm_cq: %p, efa_rdm_ep: %p, qp enabled: %d\n", ibv_wc_read_qp_num(ibv_cq->ibv_cq_ex), prov_errno, efa_rdm_cq, ep, ep->base_ep.efa_qp_enabled);
 			switch (opcode) {
 			case IBV_WC_SEND: /* fall through */
 			case IBV_WC_RDMA_WRITE: /* fall through */
@@ -673,6 +675,8 @@ int efa_rdm_cq_open(struct fid_domain *domain, struct fi_cq_attr *attr,
 
 	efa_domain = container_of(domain, struct efa_domain,
 				  util_domain.domain_fid);
+
+	EFA_WARN(FI_LOG_CQ, "created efa_rdm_cq %p, efa_domain: %p\n", cq, efa_domain);
 	/* Override user cq size if it's less than recommended cq size */
 	attr->size = MAX(efa_domain->rdm_cq_size, attr->size);
 
