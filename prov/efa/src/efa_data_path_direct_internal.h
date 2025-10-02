a13ae82babc686c6dedd023917908d61877df2d2
Shi Jin <sjina@amazon.com>
2025-10-02 21:38:42 +0000

    prov/efa: make efa_data_path_direct_send_wr_post_working not-inline
    for debugging purpose only
    
    Signed-off-by: Shi Jin <sjina@amazon.com>

--- prov/efa/src/efa_data_path_direct_internal.h
+++ prov/efa/src/efa_data_path_direct_internal.h
@@ -565,35 +565,8 @@ EFA_ALWAYS_INLINE void efa_send_wr_set_rdma_addr(struct efa_io_tx_wqe *tx_wqe,
 	remote_mem->buf_addr_hi = remote_addr >> 32;
 }
 
-EFA_ALWAYS_INLINE void
-efa_data_path_direct_send_wr_post_working(struct efa_qp *qp,
-					  struct efa_data_path_direct_sq *sq,
-					  bool force_doorbell)
-{
-	uint32_t sq_desc_idx;
-	uint64_t *src, *dst;
-
-	sq_desc_idx = (sq->wq.pc - 1) & sq->wq.desc_mask;
-
-	src = (uint64_t *)&sq->curr_tx_wqe;
-	dst = (uint64_t *)((struct efa_io_tx_wqe *)sq->desc + sq_desc_idx);
-
-	/* Copy 64-byte WQE using 8 uint64_t stores */
-	for (int i = 0; i < 8; i++)
-		dst[i] = src[i];
-
-	/* this routine only rings the doorbell if it must. */
-	if (force_doorbell) {
-		mmio_flush_writes();
-		efa_sq_ring_doorbell(sq, sq->wq.pc);
-		mmio_wc_start();
-		sq->num_wqe_pending = 0;
-	}
-
-#if HAVE_LTTNG
-	efa_data_path_direct_tracepoint_post_send(qp,sq);
-#endif
-}
+void efa_data_path_direct_send_wr_post_working(struct efa_data_path_direct_sq *sq,
+					       bool force_doorbell);
 
 EFA_ALWAYS_INLINE struct efa_io_tx_wqe *
 efa_data_path_direct_send_wr_common(struct efa_qp *qp,
