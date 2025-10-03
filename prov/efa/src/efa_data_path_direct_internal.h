5289ac7581012b086b869ce4f30a5095dc1f30d5
Shi Jin <sjina@amazon.com>
2025-10-03 18:33:05 +0000

    prov/efa: optimize the WQE memcpy
    Using unrolled assignments

--- prov/efa/src/efa_data_path_direct_internal.h
+++ prov/efa/src/efa_data_path_direct_internal.h
@@ -570,10 +570,22 @@ efa_data_path_direct_send_wr_post_working(struct efa_data_path_direct_sq *sq,
 					  bool force_doorbell)
 {
 	uint32_t sq_desc_idx;
+	uint64_t *src, *dst;
 
 	sq_desc_idx = (sq->wq.pc - 1) & sq->wq.desc_mask;
-	mmio_memcpy_x64((struct efa_io_tx_wqe *)sq->desc + sq_desc_idx,
-			&sq->curr_tx_wqe, sizeof(struct efa_io_tx_wqe));
+	src = (uint64_t *)&sq->curr_tx_wqe;
+	dst = (uint64_t *)((struct efa_io_tx_wqe *)sq->desc + sq_desc_idx);
+
+	/* Copy 64-byte WQE using 8 unrolled stores */
+	dst[0] = src[0];
+	dst[1] = src[1];
+	dst[2] = src[2];
+	dst[3] = src[3];
+	dst[4] = src[4];
+	dst[5] = src[5];
+	dst[6] = src[6];
+	dst[7] = src[7];
+
 	/* this routine only rings the doorbell if it must. */
 	if (force_doorbell) {
 		mmio_flush_writes();
