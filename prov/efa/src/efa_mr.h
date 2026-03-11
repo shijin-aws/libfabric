fbcfa2a9a9944b947f0febee6064247c7ad5da5c
Shi Jin <sjina@amazon.com>
2026-03-11 22:36:35 +0000

    prov/efa: move mr attr validation into a helper function
    Signed-off-by: Shi Jin <sjina@amazon.com>

--- prov/efa/src/efa_mr.h
+++ prov/efa/src/efa_mr.h
@@ -56,6 +56,9 @@ int efa_mr_cache_entry_reg(struct ofi_mr_cache *cache,
 void efa_mr_cache_entry_dereg(struct ofi_mr_cache *cache,
 			      struct ofi_mr_entry *entry);
 
+
+int efa_mr_regattr_validate(struct fid *fid, const struct fi_mr_attr *attr, uint64_t flags);
+
 static inline bool efa_mr_is_hmem(struct efa_rdm_mr *efa_rdm_mr)
 {
 	return efa_rdm_mr && (
